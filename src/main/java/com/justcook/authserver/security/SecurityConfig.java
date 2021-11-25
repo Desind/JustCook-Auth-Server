36a6ee163d98cffd5ab68fc6c2943dabcd608f60
Desind <zaran1998@gmail.com>
2021-11-25 12:19:17 +0100

    +cors fix

--- src/main/java/com/justcook/authserver/security/SecurityConfig.java
+++ src/main/java/com/justcook/authserver/security/SecurityConfig.java
@@ -54,6 +54,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().antMatchers(AUTH_WHITELIST).permitAll();

60c4c31052d2f9453a045064b27e124d287e29dc
Arkadiusz Grzywa <arrrusssdb@gmail.com>
2021-12-06 19:46:48 +0100

    Diff levels

--- src/main/java/com/justcook/authserver/model/Recipe/RecipeCuisine.java
+++ src/main/java/com/justcook/authserver/model/Recipe/RecipeCuisine.java
@@ -26,4 +26,5 @@ public enum RecipeCuisine {
     CARIBBEAN,
     MEDITERRANEAN,
     JAPANESE,
+    POLISH
 }
--- src/main/java/com/justcook/authserver/model/Recipe/RecipeDifficulty.java
+++ src/main/java/com/justcook/authserver/model/Recipe/RecipeDifficulty.java
@@ -1,7 +1,10 @@
 package com.justcook.authserver.model.Recipe;
 
 public enum RecipeDifficulty {
+    BEGGINER,
     EASY,
+    AVERAGE,
     MEDIUM,
+    ADVANCED,
     HARD
 }
